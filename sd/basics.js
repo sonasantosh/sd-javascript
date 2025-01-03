294ebe6c74eccb2057b7c9c4d4c23154fc713dc4
Santosh Kumar <51169582+sonasantosh@users.noreply.github.com>
2025-01-03 17:52:09 +0000

    Added dev container and basic of js.

---
+++ sd/basics.js
@@ -0,0 +1,10 @@
+const userId = 123456;
+let userName = "Santosh Kumar";
+var userEmail = "skumar@test.com";
+userContact = "9876543210";
+
+// userId = 10; // Not allowed due to constant.
+
+// console.log(userId);
+
+console.table([userId, userName, userEmail, userContact]);
